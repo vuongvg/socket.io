471cf28bd5d62f29fb776d06a69ab744e45f6ac9
vuongcdt <vuongcdt@gmail.com>
2022-12-04 13:25:49 +0700

    test disconnect fix io

--- index.js
+++ index.js
@@ -13,9 +13,14 @@ const port = process.env.PORT || 5001;
 
 var app = require("express")();
 var http = require("http").Server(app);
-var io = require("socket.io")(http);
+var io = require("socket.io")(http,{
+   transports: ['websocket'],
+   pingInterval: 1000 * 60 * 5,
+   pingTimeout: 1000 * 60 * 3
+ });
 
-app.use(morgan('dev'))
+// app.use(morgan('dev'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms [:date[web]]'))
 app.use(express.json());
 app.use(cors(false));
 
@@ -24,18 +29,18 @@ let i=0
 app.get("/", function (req, res) {
    res.send('sever run')
 });
-app.get("/start", function (req, res) {
-   time=setInterval(() => {
-      i++
-      console.log('time: ',new Date().toLocaleString())
-   }, 5*60*1000);
-   res.send('sever start time')
-});
-app.get("/stop", function (req, res) {
-   console.log('i',i)
-   clearInterval(time)
-   res.send('sever stop time: i '+i)
-});
+// app.get("/start", function (req, res) {
+//    time=setInterval(() => {
+//       i++
+//       console.log('time: ',new Date().toLocaleString())
+//    }, 5*60*1000);
+//    res.send('sever start time')
+// });
+// app.get("/stop", function (req, res) {
+//    console.log('i',i)
+//    clearInterval(time)
+//    res.send('sever stop time: i '+i)
+// });
 
 app.use("/api", router);
 app.use(notFoundMdw); 
